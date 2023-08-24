e1b87890297442e3646d522827033829f45890a4
wulinlin <wll@wisoft.com.cn>
2023-08-24 16:54:10 +0800

    1

--- source/vue/xzs-admin/src/api/task.js
+++ source/vue/xzs-admin/src/api/task.js
@@ -4,5 +4,6 @@ export default {
   pageList: query => post('/api/admin/task/page', query),
   edit: query => post('/api/admin/task/edit', query),
   select: id => post('/api/admin/task/select/' + id),
+  exportList: query => post('/api/admin/task/export', query),
   deleteTask: id => post('/api/admin/task/delete/' + id)
 }
